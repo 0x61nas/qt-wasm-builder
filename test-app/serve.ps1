0847007d553e7ec6b5d945279b70a8ce31896867
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2026-06-09 17:44:12 +0300

    chore(test): fix the serve msg

--- test-app/serve.ps1
+++ test-app/serve.ps1
@@ -2,11 +2,11 @@
 echo `# <#`
 PORT="${1:-8081}"
 cd "$(dirname "$0")/build/wasm"
-echo "Serving CommTrace Viewer WASM build on http://localhost:$PORT"
+echo "Serving Test application WASM build on http://localhost:$PORT"
 python3 -m http.server "$PORT"
 exit 0
 #> > $null
 param($Port=8081)
 Set-Location "$PSScriptRoot\build\wasm"
-Write-Host "Serving CommTrace Viewer WASM build on http://localhost:$Port"
+Write-Host "Serving Test application WASM build on http://localhost:$Port"
 python -m http.server $Port
